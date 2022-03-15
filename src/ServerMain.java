afd337bc447191db9e52d312645372bd9879a30c
X0lotl <92333793+X0lotl@users.noreply.github.com>
2022-03-15 10:15:43 +0200

    finish Hello World http SoccketServer

--- src/ServerMain.java
+++ src/ServerMain.java
@@ -2,17 +2,40 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.Buffer;
 import java.nio.charset.StandardCharsets;
 
 public class ServerMain {
     public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000),0);
-        server.createContext("/test", new MyHandler());
-        server.setExecutor(null);
-        server.start();
+        int port = 8000;
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.err.println("Server starts on port: " + port);
+
+        while(true){
+            Socket clientSocket = serverSocket.accept();
+            System.err.println("New client connect");
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+            String newString;
+            while ((newString = bufferedReader.readLine()) != null){
+                System.out.println(newString);
+                if (newString.isEmpty()){
+                    break;
+                }
+            }
+
+            bufferedWriter.write("HelloWorld!");
+            System.out.println("Client connection terminate");
+            bufferedWriter.close();
+            bufferedReader.close();
+            clientSocket.close();
+        }
     }
 
     static class MyHandler implements HttpHandler {
