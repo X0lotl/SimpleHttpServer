238df796e90086da3c6407e8ba3b4b047498d498
X0lotl <92333793+X0lotl@users.noreply.github.com>
2022-03-15 22:40:03 +0200

    delete imports in main

--- src/ServerMain.java
+++ src/ServerMain.java
@@ -1,21 +1,10 @@
-import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpsServer;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-public class ServerMain{
+
+public class ServerMain {
     private static Server createServer() {
         RequestHandler requestHandler = new RequestHandler();
         Server server = new Server() {
@@ -23,7 +12,7 @@ public class ServerMain{
             public void start(int port) throws IOException {
                 try (ServerSocket serverSocket = new ServerSocket(port)) {
                     while (true) {
-                        try (Socket client = serverSocket.accept()){
+                        try (Socket client = serverSocket.accept()) {
                             requestHandler.handleRequest(client);
                         }
                     }
