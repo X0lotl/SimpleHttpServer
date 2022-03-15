b35fd0162fe004f26d07eaba6bca0633f27c6de1
X0lotl <92333793+X0lotl@users.noreply.github.com>
2022-03-15 12:50:35 +0200

    add parsing messeges and html files but it has bag in output files

--- src/ServerMain.java
+++ src/ServerMain.java
@@ -1,51 +1,88 @@
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-
-import java.io.*;
-import java.net.InetSocketAddress;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.Buffer;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ServerMain {
     public static void main(String[] args) throws IOException {
-        int port = 8000;
-        ServerSocket serverSocket = new ServerSocket(port);
-        System.err.println("Server starts on port: " + port);
-
-        while(true){
-            Socket clientSocket = serverSocket.accept();
-            System.err.println("New client connect");
-
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-
-            String newString;
-            while ((newString = bufferedReader.readLine()) != null){
-                System.out.println(newString);
-                if (newString.isEmpty()){
-                    break;
+        try (ServerSocket serverSocket = new ServerSocket(8000)) {
+            while (true) {
+                try (Socket client = serverSocket.accept()) {
+                    handleClient(client);
                 }
             }
-
-            bufferedWriter.write("HelloWorld!");
-            System.out.println("Client connection terminate");
-            bufferedWriter.close();
-            bufferedReader.close();
-            clientSocket.close();
         }
     }
 
-    static class MyHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange exchange) throws IOException {
-            String response = "Hello World!";
-            exchange.sendResponseHeaders(200,response.length());
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.getBytes(StandardCharsets.UTF_8));
-            os.close();
+    private static void handleClient(Socket client) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+        StringBuilder requestBuilder = new StringBuilder();
+        String line;
+
+        while (!(line = bufferedReader.readLine()).isBlank()) {
+            requestBuilder.append(line + "\r\n");
         }
+
+        String request = requestBuilder.toString();
+
+        String[] requestsLines = request.split("\r\n");
+        String[] firstRequestLine = requestsLines[0].split(" ");
+        String method = firstRequestLine[0];
+        String path = firstRequestLine[1];
+        String version = firstRequestLine[2];
+        String host = requestsLines[1].split(" ")[1];
+
+        List<String> headers = new ArrayList<>(Arrays.asList(requestsLines).subList(2, requestsLines.length));
+
+        String accessLog = String.format("Client %s, method %s, path %s, version %s, host %s, headers %s",
+                client.toString(), method, path, version, host, headers.toString());
+        System.out.println(accessLog);
+
+        Path filePath = getFilePath(path);
+
+        if (Files.exists(filePath)) {
+            String contentType = guessContentType(filePath);
+            sendResponse(client, "200 OK", contentType, Files.readAllBytes(filePath));
+        } else {
+            byte[] notFoundContent = "<h1>404 Not found :(</h1>".getBytes();
+            sendResponse(client,"404 Not Found", "text/html", notFoundContent);
+        }
+    }
+
+    private static void sendResponse(Socket client, String status, String contentType, byte[] content) throws IOException {
+        OutputStream clientOutput = client.getOutputStream();
+        clientOutput.write(("HTTP/1.1 \r\n" + status).getBytes());
+        clientOutput.write(("ContentType: " + contentType + "\r\n").getBytes());
+        clientOutput.write("\r\n".getBytes());
+        clientOutput.write(content);
+        clientOutput.write("\r\n\r\n".getBytes());
+        clientOutput.flush();
+        clientOutput.close();
+    }
+
+    private static Path getFilePath(String path) {
+        if ("/hello".equals(path)) {
+            path = "src/site/html/hello.html";
+        } else if ("/meme".equals(path)) {
+            path = "src/site/html/meme.html";
+        } else {
+            path = "src/site/html/main.html";
+        }
+
+        return Paths.get("/tmp/www", path);
+    }
+
+    private static String guessContentType(Path filePath) throws IOException {
+        return Files.probeContentType(filePath);
     }
 }
