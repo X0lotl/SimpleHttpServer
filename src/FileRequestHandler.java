62c3ec29c1c982247a39d7bbc44205e93c8ab595
X0lotl <92333793+X0lotl@users.noreply.github.com>
2022-03-16 20:21:40 +0200

    Change FileRequestHandler to record

--- src/FileRequestHandler.java
+++ src/FileRequestHandler.java
@@ -1,11 +1,7 @@
 import java.io.*;
 import java.nio.file.*;
 
-public class FileRequestHandler implements RequestHandler {
-    private final String pathOfDirectory;
-    public FileRequestHandler(String pathOfDirectory){
-        this.pathOfDirectory = pathOfDirectory;
-    }
+public record FileRequestHandler(String pathOfDirectory) implements RequestHandler {
 
     @Override
     public void handleRequest(RequestData requestData, Response response) throws IOException {
@@ -14,11 +10,11 @@ public class FileRequestHandler implements RequestHandler {
         Path filePath = path.resolve(requestData.path().substring(1));
         File file = new File(String.valueOf(filePath));
 
-        if(file.exists()){
-            response.send("200", Files.readAllBytes(filePath),requestData.headers());
+        if (file.exists()) {
+            response.send("200", Files.readAllBytes(filePath), requestData.headers());
         } else {
             byte[] notFoundContent = "<h1>404 Not found :(</h1>".getBytes();
-            response.send("404",notFoundContent,requestData.headers());
+            response.send("404", notFoundContent, requestData.headers());
         }
 
     }
